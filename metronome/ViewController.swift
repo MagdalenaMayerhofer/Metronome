0ad427996fbc470cbe262ea0c78cf039a300c0c6
Magdalena Mayerhofer <m.mayerhofer@student.tugraz.at>
2019-03-08 09:01:50 +0100

    Initial Commit

---
+++ metronome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  metronome
+//
+//  Created by Magdalena Mayerhofer on 08.03.19.
+//  Copyright © 2019 Magdalena Mayerhofer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
