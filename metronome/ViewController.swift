bde66ed7c5c81e8f79ff0cdb644abb30ca5020c4
Magdalena Mayerhofer <m.mayerhofer@student.tugraz.at>
2019-03-08 10:53:29 +0100

    ADD basic design

--- metronome/ViewController.swift
+++ metronome/ViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var bpmLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func bpmSliderValueChanged(_ sender: UISlider) {
+        bpmLabel.text = "\(Int(sender.value)) BPM"
+    }
 }
 
